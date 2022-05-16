f8809eb465c0cd97020ba5652bd80e9ea0ef94ed
bhaleraoneha <99394922+bhaleraoneha@users.noreply.github.com>
2022-05-17 00:55:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express =require('express');
+
+
+const app = express ();
+
+const PORT = 5000;
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, ()  =>{
+      console.log("port running on 5000");
+})
